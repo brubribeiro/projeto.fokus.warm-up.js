e79de3b152436f0f923e977c53afd47e6f677dcf
Bruna Ribeiro <bruna.ribeiro@facens.br>
2025-03-11 21:50:27 -0300

    feat(script): added audio files

--- script.js
+++ script.js
@@ -10,26 +10,47 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const iniciarBt = document.querySelector('#start-pause');
+const buttons = document.querySelectorAll('.app__card-button');
 
-const duracaoFoco = 1500; 
-const duracaoDescansoCurto = 300; 
-const duracaoDescansoLongo = 900; 
+const musicFocoInput = document.querySelector('#alternar-musica');
+
+const music = new Audio('/sons/luna-rise-part-one.mp3');
+
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
+music.loop = true;
 
 focoBt.addEventListener('click', () => {
   changeContexto('foco');
+  focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
   changeContexto('descanso-curto');
+  curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
   changeContexto('descanso-longo');
+  longoBt.classList.add('active');
+});
+
+musicFocoInput.addEventListener('change', () => {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
 });
 
 function changeContexto(contexto) {
   html.setAttribute('data-contexto', contexto);
   image.setAttribute('src', `/imagens/${contexto}.png`);
+  buttons.forEach(function (bt) { 
+    bt.classList.remove('active') 
+  });
 
   switch (contexto) {
     case "foco":
@@ -39,7 +60,7 @@ function changeContexto(contexto) {
     case "descanso-curto":
       title.innerHTML = `Que tal dar uma respirada?<br><strong class='app__title-strong'>Faça uma pausa curta!</strong>`;
       break;
-  
+
     case "descanso-longo":
       title.innerHTML = `Hora de voltar à superfície.<br><strong class='app__title-strong'>Faça uma pausa longa.</strong>`;
       break;
