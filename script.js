7b411db8eb8354a3884733b163d8c7e6583a7dee
Bruna Ribeiro <bruna.ribeiro@facens.br>
2025-03-11 22:21:32 -0300

    feat(script): added start and pause timer

--- script.js
+++ script.js
@@ -9,18 +9,27 @@ const timer = document.querySelector('#timer');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
-const iniciarBt = document.querySelector('#start-pause');
+const startPauseBt = document.querySelector('#start-pause');
 const buttons = document.querySelectorAll('.app__card-button');
+
+const startOrPauseBt = document.querySelector('#start-pause span');
+const startOrPauseImg = document.querySelector('#start-pause img');
 
 const musicFocoInput = document.querySelector('#alternar-musica');
 
 const music = new Audio('/sons/luna-rise-part-one.mp3');
+music.loop = true;
+
+const playMusic = new Audio('/sons/play.wav');
+const pauseMusic = new Audio('/sons/pause.mp3');
+const finishedMusic = new Audio('/sons/beep.mp3');
 
 const duracaoFoco = 1500;
 const duracaoDescansoCurto = 300;
 const duracaoDescansoLongo = 900;
 
-music.loop = true;
+let tempoDecorridoEmSegundos = 5;
+let intervalId = null;
 
 focoBt.addEventListener('click', () => {
   changeContexto('foco');
@@ -65,4 +74,38 @@ function changeContexto(contexto) {
       title.innerHTML = `Hora de voltar à superfície.<br><strong class='app__title-strong'>Faça uma pausa longa.</strong>`;
       break;
   }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    finishedMusic.play();
+    alert('Tempo finalizado!');
+    reset();
+    return;
+  }
+
+  tempoDecorridoEmSegundos -= 1;
+}
+
+startPauseBt.addEventListener('click', startOrPause);
+
+
+function startOrPause() {
+  if (intervalId) {
+    pauseMusic.play();
+    reset();
+    return;
+  }
+
+  playMusic.play();
+  intervalId = setInterval(contagemRegressiva, 1000);
+  startOrPauseBt.textContent = 'Pausar';
+  startOrPauseImg.setAttribute('src', '/imagens/pause.png');
+}
+
+function reset() {
+  clearInterval(intervalId);
+  startOrPauseBt.textContent = 'Começar';
+  startOrPauseImg.setAttribute('src', '/imagens/play_arrow.png');
+  intervalId = null;
 }
